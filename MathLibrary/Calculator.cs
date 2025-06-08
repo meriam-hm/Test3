bda77c10537e735c40f11b112762c52a382855c1
meriam <meriamslm96@gmail.com>
2025-06-08 01:36:35 -0700

    The example Calculator.Multiply has been implemented

---
+++ MathLibrary/Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace MathLibrary;
+
+    /// <summary>
+    /// يحتوي على دوال رياضيه بسيطه
+    /// </summary>
+    public static class Calculator
+    {
+        /// <summary>
+        /// تحسب حاصل ضرب عددين
+        /// </summary>
+        /// <param name="a">العدد الأول</param>
+        /// <param name="b">العدد الثاني</param>
+        /// <returns>ناتج الضرب</returns>
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+
